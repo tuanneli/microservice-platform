dcc4bd7ab40c62ccdf37919716f995e6a6307897
tuanneli <zhuravlev99pic@gmail.com>
2023-03-11 16:23:07 +0300

    add mode (development|production)

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ const program = require('commander')
 
 program
     .option('-P, --port [port]', 'Server dev port', 4000)
+    .option('-M, --mode [mode]', 'Server mode', /^(development|production)$/i, 'development')
     .option('-O, --open-browser', 'Initialize browser on start')
     .parse(process.argv)
 
 const options = program.opts()
+
+process.env.NODE_ENV = options.mode
 
 require('./app.js')(options)
